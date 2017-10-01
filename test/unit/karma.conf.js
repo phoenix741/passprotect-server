9ee15cfe23a342c7bc1446fe306f6690fbe09af8
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2017-10-01 21:25:52 +0200

    Update the drone configuration file

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -3,7 +3,8 @@
 // we are also using it with karma-webpack
 //   https://github.com/webpack/karma-webpack
 
-var webpackConfig = require('../../build/webpack.test.conf')
+const webpackConfig = require('../../build/webpack.test.conf')
+const isDocker = require('is-docker')()
 
 module.exports = function (config) {
   config.set({
@@ -11,7 +12,7 @@ module.exports = function (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeCustom'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
@@ -30,6 +31,12 @@ module.exports = function (config) {
         { type: 'text-summary' },
         { type: 'html' }
       ]
+    },
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeHeadless',
+        flags: isDocker ? ['--no-sandbox'] : []
+      }
     }
   })
 }
